34d06bf53e340317a9d9f30d0a151682450f3dff
Madhumitha <Madhumitha_loganathan@intuit.com>
2017-09-19 16:20:58 -0700

    script changes

--- socketio+affectiva/script.sh
+++ socketio+affectiva/script.sh
@@ -8,8 +8,7 @@ stop on shutdown
  
 script
     exec ffmpeg -s 320x240 -f video4linux2 -i /dev/video0 \
- -f mpeg1video -b 400k -r 30 \
- -vf "drawtext=fontfile=/usr/share/fonts/truetype/ttf-dejavu/DejaVuSans.ttf: text='MyContraption.com': fontsize=10:fontcolor=white@1.0: box=1: boxcolor=black@0.5: x=7: y=220, drawtext=fontfile=/usr/share/fonts/truetype/ttf-dejavu/DejaVuSans.ttf: text='%{localtime} EST (GMT -6)': fontsize=10:fontcolor=white@1.0: box=1: boxcolor=black@0.5: x=(w-175): y=220" http://localhost:8082/SecretCodeGoesHere/320/240
+ -f mpeg1video -b 400k -r 30 http://localhost:8082/SecretCodeGoesHere/320/240
 end script
  
 pre-start script
