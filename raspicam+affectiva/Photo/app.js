adf0df03f7b88a331745402bbd5e8e110fc48901
Madhumitha <Madhumitha_loganathan@intuit.com>
2017-09-19 13:00:14 -0700

    require

--- raspicam+affectiva/Photo/app.js
+++ raspicam+affectiva/Photo/app.js
@@ -15,3 +15,41 @@ app.listen(3000, function () {
 app.get('/index', function(req, res) {
    res.sendFile(path.join(__dirname + '/index.html'));
 });
+
+
+var RaspiCam = require("raspicam");
+
+
+var camera = new RaspiCam({
+  mode: "photo",
+  output: "./photo/image.jpg",
+  encoding: "jpg",
+  timeout: 100 // take the picture immediately
+});
+
+// var camera1 = new RaspiCam({
+//  mode: "video",
+//  output: "./video/video.h264",
+//  framerate: 15,
+//  timeout: 5000 // take a 5 second video
+// });
+
+
+camera.on("start", function( err, timestamp ){
+  console.log("photo started at " + timestamp );
+});
+
+camera.on("read", function( err, timestamp, filename ){
+  console.log("photo image captured with filename: " + filename );
+});
+
+camera.on("exit", function( timestamp ){
+  console.log("photo child process has exited at " + timestamp );
+});
+
+camera.start();
+console.log("about to wait...");
+setTimeout(function() {
+  console.log("done waiting");
+  camera.stop();
+}, 20000);
