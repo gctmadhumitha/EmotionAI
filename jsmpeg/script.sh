d8fa4ae01f0cc9b455daca426e9e1e1b57e9cd81
Madhumitha <Madhumitha_loganathan@intuit.com>
2017-09-20 15:21:06 -0700

    change scripts

--- jsmpeg/script.sh
+++ jsmpeg/script.sh
@@ -1,7 +1,7 @@
 # /etc/init/jsmpeg-webcam-server.conf
  
 description "Webcam streaming server using jsmpeg and node.js"
-author      "MyContraption.com"
+author      "Madhu"
  
 start on local-filesystems and net-device-up IFACE!=eth0
 stop on shutdown
@@ -12,7 +12,7 @@ script
 end script
  
 pre-start script
-    echo "[`date`] Starting ffmpeg video capture" >> /home/pi/Devel/Webcam/jsmpeg/jsmpg.log
+    echo "[`date`] Starting ffmpeg video capture" >> /home/pi/MM/code/trials/jsmpeg/jsmpg.log
 end script
  
 pre-stop script
